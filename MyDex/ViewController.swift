3419ec798a09f3a51d91ede5cc1b585bb1b0038c
Eric Dockery <Eric.Dockery283@gmail.com>
2020-03-21 14:08:54 -0600

    Initial Commit

---
+++ MyDex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyDex
+//
+//  Created by Eric Dockery on 3/21/20.
+//  Copyright © 2020 Eric Dockery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
